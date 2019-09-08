cc1f01c04977dd314a27f993c4f8e9260d86084e
Sho Kusano <rosylilly@aduca.org>
2019-09-08 17:39:31 +0900

    More less category

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -182,7 +182,6 @@ module Isucari
           ids[get_current_user_id % ids.length],
           ids[(get_current_user_id + 1) % ids.length],
           ids[(get_current_user_id + 2) % ids.length],
-          ids[(get_current_user_id + 3) % ids.length],
         ]
       end
 
