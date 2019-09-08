e0987b5a5d6b008b4b29dbdda3c7dfa4b1605fc0
Issei Naruta <naruta@cookpad.com>
2019-09-08 11:06:05 +0900

    Introduce NewRelic

--- webapp/ruby/lib/isucari/web.rb
+++ webapp/ruby/lib/isucari/web.rb
@@ -19,7 +19,7 @@ class Mysql2ClientWithNewRelic < Mysql2::Client
       NewRelic::Agent::Datastores.notice_sql(sql, metrics, elapsed)
     end
     op = sql[/^(select|insert|update|delete)/i] || 'other'
-    table = sql[/\breservations|sheets|events\b/]
+    table = sql[/\bcategories|configs|items|shippings|transaction_evidences|users\b/] || 'other'
     NewRelic::Agent::Datastores.wrap('MySQL', op, table, callback) do
       super
     end
