f0f45b67b4edc9ee6730cd00061ebac03196510b
Sorah Fukumori <sorah@cookpad.com>
2019-09-08 10:55:11 +0900

    nginx.conf

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+selfsum="$(openssl dgst -sha256 "$0")"
+#export PATH=/home/isucon/local/ruby/bin:$PATH
+#
+cd ~/git/webapp/ruby/
+git pull --rebase
+if [ "_${selfsum}" != "_$(openssl dgst -sha256 "$0")" ]; then
+  exec $0
+fi
+
+sudo cp ~/git/systemd/* /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl restart isucari.ruby.service
+
+sudo cp ~/git/nginx.conf /etc/nginx/nginx.conf
+
+sudo nginx -t
+sudo nginx -s reload || :
+
+sudo bash -c 'cp /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%s) && echo > /var/log/nginx/access.log; echo > /tmp/isu-query.log; echo > /tmp/isu-rack.log; test -d /tmp/stackprof && rm -f /tmp/stackprof/*; echo > /var/lib/mysql/mysql-slow.log'
